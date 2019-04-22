cd838076150e15ae55e6e667fb0bcf3c1b15cd11
Владимир <vladimir1860921@gmail.com>
2019-04-22 22:55:43 +0300

    Добавьте файлы проекта.

---
+++ SecondCode/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+
+
+namespace SecondCode
+{
+    public class Book
+    {
+        public int id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+
+    }
+
+    class BookContext : DbContext
+    {
+
+        public BookContext() : base("UserDB")
+        {
+
+        }
+
+        public DbSet<Book> booklist { set; get; }
+
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (BookContext bk = new BookContext())
+            {
+                var books = bk.booklist;
+                foreach (Book b in books)
+                {
+                    Console.WriteLine(b.id + " " + b.Title + " " + b.Author);
+                }
+
+                Console.ReadKey();
+            }
+
+
+        }
+    }
+}
